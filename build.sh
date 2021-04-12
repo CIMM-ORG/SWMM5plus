edb3df902adcf3d75b7eb9bd2622a76e3f61fe5b
gr24269 <griano@utexas.edu>
2021-04-12 11:59:22 -0500

    New build

--- build.sh
+++ build.sh
@@ -10,3 +10,26 @@ then
     mv src/*.* .
     rm -r src
 fi
+
+INIT_DIR='initialization'
+JSON_DIR='json-fortran'
+VARS_DIR='vars'
+
+FC='gfortran-9'
+OUPTFLAGS=-g
+FFLAGS=-O3
+PROGRAM=SWMM
+PROGRAM_OBJ=$PROGRAM.o
+
+SOURCESF="$INIT_DIR/dll.f08\
+            $INIT_DIR/setting_definition.f08\
+            $INIT_DIR/interface.f08"
+
+echo Compiling ...
+$FC $SOURCESF -ldl -o $PROGRAM
+
+$clean:
+    echo Clean Object files...
+    rm -rf *.o *.mod
+
+echo Complete!
