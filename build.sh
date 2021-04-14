21a0c22779a96d581ccbeb5600ae7652b57a70e5
gr24269 <griano@utexas.edu>
2021-04-14 10:23:11 -0500

    Changes to the build.sh

--- build.sh
+++ build.sh
@@ -21,9 +21,17 @@ FFLAGS=-O3
 PROGRAM=SWMM
 PROGRAM_OBJ=$PROGRAM.o
 
-SOURCESF="$INIT_DIR/dll.f08\
-            $INIT_DIR/setting_definition.f08\
-            $INIT_DIR/interface.f08"
+SOURCESF="$JSON_DIR/json_kinds.F90\
+          $JSON_DIR/json_parameters.F90\
+          $JSON_DIR/json_string_utilities.F90\
+          $JSON_DIR/json_value_module.F90\
+          $JSON_DIR/json_file_module.F90\
+          $JSON_DIR/json_module.F90\
+          $VARS_DIR/data_keys.f08\
+          $VARS_DIR/type_definitions.f08\
+          $INIT_DIR/setting_definition.f08\
+          $VARS_DIR/globals.f08\
+          main.f08"
 
 echo Compiling ...
 $FC $SOURCESF -ldl -o $PROGRAM
