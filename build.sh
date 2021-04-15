a5448d7723584efbff28105e52103a6747494c9f
gr24269 <griano@utexas.edu>
2021-04-15 15:15:29 -0500

    Updated build.sh
    The build.sh had to be modified to handle
    the new file locations considering that
    SWMMengine files have been reorganized in folders.
    
    The build.sh is now fool-proof, meaning
    that we don't have to care anymore about commenting
    lines to avoid downloading unnecessary files, e.g.,
    SWMM C library. Just run ./build.sh
    
    Make sure that your build.sh has execution rights by
    running:
    
    chmod +x build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
-# Download dependencies
 shopt -s extglob
+
+# Constants
+
+ARRAY_DIR='arrays'
+INIT_DIR='initialization'
+API_DIR='initialization/interface'
+JSON_DIR='json-fortran'
+SIM_DIR='simulation'
+TEST_DIR='test_cases'
+UTIL_DIR='utilities'
+VARS_DIR='vars'
+
+FC='gfortran-9'
+OUPTFLAGS=-g
+FFLAGS=-O3
+PROGRAM=SWMM
+DEBUG=true
+
+if [ $DEBUG = true ]
+then
+    rm -r debug
+    mkdir debug
+fi
+
+# Download dependencies
+
 if [ ! -d 'json-fortran' ]
 then
     git clone 'https://github.com/jacobwilliams/json-fortran.git'
@@ -11,15 +36,33 @@ then
     rm -r src
 fi
 
-INIT_DIR='initialization'
-JSON_DIR='json-fortran'
-VARS_DIR='vars'
+# Download SWMM C
 
-FC='gfortran-9'
-OUPTFLAGS=-g
-FFLAGS=-O3
-PROGRAM=SWMM
-PROGRAM_OBJ=$PROGRAM.o
+if [ ! -d "$API_DIR/src" ]
+then
+    echo Downloading SWMM from US EPA repository ...
+    wget "https://github.com/USEPA/Stormwater-Management-Model/archive/v5.1.13.tar.gz"
+    tar -xvf *.tar.gz
+    rm *.tar.gz
+    mv Stormwater*/src "$API_DIR/src"
+    rm -r Stormwater*
+fi
+
+# Compile SWMM C
+
+echo Compiling SWMM 5.13 ...
+cp "$API_DIR/interface.h" "$API_DIR/src/"
+cp "$API_DIR/interface.c" "$API_DIR/src/"
+cp "$API_DIR/tests.h" "$API_DIR/src/"
+cp "$API_DIR/tests.c" "$API_DIR/src/"
+cp "$API_DIR/Makefile" "$API_DIR/src/"
+cd "$API_DIR/src"
+make
+cd ..
+cp src/libswmm5.so ../../libswmm5.so
+cd ../..
+
+# Compile
 
 SOURCESF="$JSON_DIR/json_kinds.F90\
           $JSON_DIR/json_parameters.F90\
@@ -31,13 +74,22 @@ SOURCESF="$JSON_DIR/json_kinds.F90\
           $VARS_DIR/type_definitions.f08\
           $INIT_DIR/setting_definition.f08\
           $VARS_DIR/globals.f08\
+          $API_DIR/dll.f08\
+          $UTIL_DIR/datetime.f08\
+          $ARRAY_DIR/dynamic_array.f08\
+          $ARRAY_DIR/tables.f08\
+          $API_DIR/interface.f08\
+          $VARS_DIR/array_index.f08\
+          $UTIL_DIR/utility.f08\
+          $INIT_DIR/allocate_storage.f08\
+          $INIT_DIR/initialization.f08\
           main.f08"
 
 echo Compiling ...
 $FC $SOURCESF -ldl -o $PROGRAM
 
 $clean:
-    echo Clean Object files...
-    rm -rf *.o *.mod
+    echo Clean Object files ...
+    rm -rf *.o *.mod *.out
 
 echo Complete!
