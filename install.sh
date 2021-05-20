8839c5a8c261be2499b3aa1c7820882d02640675
Cheng-Wei Yu <yuchanway@utexas.edu>
2021-05-20 16:49:05 -0500

    cmake opencoarray version update

--- install.sh
+++ install.sh
@@ -74,10 +74,10 @@ then
     mkdir $CMAKE_SOURCE
     cd $CMAKE_SOURCE
     mkdir $CMAKE_INSTALL
-    wget "https://cmake.org/files/v3.3/cmake-3.3.2.tar.gz"  # use 3.3.2 for now. Versions: https://cmake.org/files/
+    wget "https://cmake.org/files/v3.11/cmake-3.11.0.tar.gz"  # use 3.3.2 for now. Versions: https://cmake.org/files/
     tar -xvf *.tar.gz
     rm *.tar.gz
-    cd cmake-3.3.2
+    cd cmake-3.11.0
     ./configure --prefix=$CMAKE_INSTALL
     make
     make install
@@ -95,7 +95,7 @@ then
     mkdir $COARRAY_INSTALL
     echo Installing Opencoarray from https://github.com/sourceryinstitute/OpenCoarrays
     sleep 3.0
-    git clone --branch 1.9.3 https://github.com/sourceryinstitute/OpenCoarrays
+    git clone https://github.com/sourceryinstitute/OpenCoarrays 
     cd OpenCoarrays
     mkdir opencoarrays-build
     cd opencoarrays-build
