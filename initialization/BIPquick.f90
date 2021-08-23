a1083410977a09f759ff6ab4333e67caad87530f
ss89697 <sazzad.sharior@utexas.edu>
2021-08-23 16:24:39 -0500

    BQuick 1 processor bypass fix

--- initialization/BIPquick.f90
+++ initialization/BIPquick.f90
@@ -60,7 +60,7 @@ contains
         !% One processor bypass for BIPquick
         if ( num_images() == 1 ) then
             node%I(:, ni_P_image) = oneI
-            node%I(:, ni_P_is_boundary) = oneI
+            node%I(:, ni_P_is_boundary) = zeroI
             link%I(:, li_P_image) = oneI
             return
         end if
