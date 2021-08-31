e6c84d03c0e58a64a94b90983ebc41c619560936
gr24269 <griano@utexas.edu>
2021-08-31 16:46:35 -0500

    Cleaned input.csv, JSON with proper paths now

--- main/main.f90
+++ main/main.f90
@@ -19,17 +19,17 @@ program main
 
     ! --- Initialization
     if (setting%Verbose) &
-        write(*,"(2A,i5.5,A)") new_line(" "), 'begin initialization [Image ', this_image(), "] ..."
+        write(*,"(2A,i5,A)") new_line(" "), 'begin initialization [Image ', this_image(), "] ..."
     call initialize_all()
 
     ! --- Time Loop
     if (setting%Verbose) &
-        write(*,"(2A,i5.5,A)") new_line(" "), 'begin timeloop [Image ', this_image(), "]"
+        write(*,"(2A,i5,A)") new_line(" "), 'begin timeloop [Image ', this_image(), "]"
     setting%Time%Real%EpochTimeLoopStartSeconds = time()
     call timeloop_toplevel()
 
     if (setting%Verbose) &
-        write(*,"(2A,i5.5,A)") new_line(" "), 'finalize [Image ', this_image(), "]"
+        write(*,"(2A,i5,A)") new_line(" "), 'finalize [Image ', this_image(), "]"
     ! --- Finalization
     call finalize_all()
 
@@ -37,8 +37,8 @@ program main
 
     call cpu_time(setting%Time%CPU%EpochFinishSeconds)
 
-    print *, new_line("")
-    write(*, "(A,i2,A,G0.6,A)") 'Processor ', this_image(), " | Simulation Time = ", &
+    write(*, "(A,i5,A,G0.6,A)") &
+        new_line(" ") // 'Processor ', this_image(), " | Simulation Time = ", &
         (setting%Time%CPU%EpochFinishSeconds - setting%Time%CPU%EpochStartSeconds), " [s]"
 
 end program main
