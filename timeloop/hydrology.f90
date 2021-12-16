a3a34ee8f4f2da30505c7fab6063875f960ed260
Ben Hodges <hodges@utexas.edu>
2021-12-16 15:23:42 -0600

    hydrology

---
+++ timeloop/hydrology.f90
@@ -0,0 +1,123 @@
+module hydrology
+
+    use define_globals
+    use define_keys
+    use define_indexes
+    use define_settings, only: setting
+    use interface
+    !use utility
+
+    implicit none
+
+    !%----------------------------------------------------------------------------- 
+    !% Description:
+    !% Handles hydrology functions that work with SWMM-C interface
+    !%
+    !% METHOD: 
+    !% 
+    !%
+
+    private
+
+    public :: hydrology_runoff
+   
+
+    contains
+!%==========================================================================
+!% PUBLIC
+!%==========================================================================
+!%
+    subroutine hydrology_runoff()
+    !     !%-------------------------------------------------------------------
+    !     !% Description:
+    !     !% Takes the hydrology runoff stored in the sbcatchR array and 
+    !     !% applies it to the lateral inflows in the elemR array for use
+    !     !% in hydraulics
+    !     !%-------------------------------------------------------------------
+    !     !% Declarations
+    !         integer :: ii 
+    !         integer, pointer :: thisE
+    !         real(8), pointer :: latFlowRate(:), subcatchFlowrate(:)
+    !         character(64) :: subroutine_name = 'hydrology_runoff'
+    !     !%-------------------------------------------------------------------
+    !     !% Preliminaries
+    !         if (icrash) return
+    !         if (setting%Debug%File%adjust) &
+    !             write(*,"(A,i5,A)") '*** enter ' // trim(subroutine_name) // " [Processor ", this_image(), "]"
+    !     !%-------------------------------------------------------------------
+    !     !% Aliases
+    !     !%-------------------------------------------------------------------              
+    !         latFlowRate      => elemR(:,er_FlowrateLateral)
+    !         subcatchFlowRate => subcatchR(:,sr_RunoffRate_baseline) 
+    !     !%-------------------------------------------------------------------  
+    !     !% Multiple subcatchments can drain to a single element, but each
+    !     !% subcatchment connects to only one element, so we cannot array
+    !     !% process between subcatchR and elemR
+    !     do ii =1,SWMM_N_subcatch
+    !         thisE => subcatchI(ii,si_runoff_elemIdx) 
+    !         latFlowRate(thisE) = latFlowRate(thisE) + subcatchFlowRate(ii)
+    !     end do
+        
+    !     !%-------------------------------------------------------------------  
+    !     !% Closing                
+    !     if (setting%Debug%File%adjust) &
+    !         write(*,"(A,i5,A)") '*** leave ' // trim(subroutine_name) // " [Processor ", this_image(), "]"
+    end subroutine hydrology_runoff
+!%
+!%==========================================================================
+!%==========================================================================  
+!%    
+    ! subroutine
+    !     !%-------------------------------------------------------------------
+    !     !% Description:
+    !     !% 
+    !     !%-------------------------------------------------------------------
+    !     !% Declarations
+    !         character(64) :: subroutine_name = 'hydro_'
+    !     !%-------------------------------------------------------------------
+    !     !% Preliminaries
+    !         if (icrash) return
+    !         if (setting%Debug%File%adjust) &
+    !             write(*,"(A,i5,A)") '*** enter ' // trim(subroutine_name) // " [Processor ", this_image(), "]"
+    !     !%-------------------------------------------------------------------
+    !     !% Aliases
+    !     !%-------------------------------------------------------------------              
+     
+        
+    !     !%-------------------------------------------------------------------  
+    !     !% Closing                
+    !     if (setting%Debug%File%adjust) &
+    !         write(*,"(A,i5,A)") '*** leave ' // trim(subroutine_name) // " [Processor ", this_image(), "]"
+    ! end subroutine 
+!%
+!%==========================================================================
+!% PRIVATE below here
+!%==========================================================================  
+!%    
+    ! subroutine
+    !     !%-------------------------------------------------------------------
+    !     !% Description:
+    !     !% 
+    !     !%-------------------------------------------------------------------
+    !     !% Declarations
+    !         character(64) :: subroutine_name = 'hydro_'
+    !     !%-------------------------------------------------------------------
+    !     !% Preliminaries
+    !         if (icrash) return
+    !         if (setting%Debug%File%adjust) &
+    !             write(*,"(A,i5,A)") '*** enter ' // trim(subroutine_name) // " [Processor ", this_image(), "]"
+    !     !%-------------------------------------------------------------------
+    !     !% Aliases
+    !     !%-------------------------------------------------------------------              
+     
+        
+    !     !%-------------------------------------------------------------------  
+    !     !% Closing                
+    !     if (setting%Debug%File%adjust) &
+    !         write(*,"(A,i5,A)") '*** leave ' // trim(subroutine_name) // " [Processor ", this_image(), "]"
+    ! end subroutine 
+!%
+!%==========================================================================
+!% END OF MODULE
+!%+=========================================================================
+end module hydrology
